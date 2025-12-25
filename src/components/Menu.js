511429e61e531d051fc2c54178fdcbd5862dbbf3
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-25 21:41:19 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-const Menu = ({ items }) => {
+function Menu({ items }) {
   return (
     <div>
       {items.map((item) => (
         <div
           key={item.id}
-          data-test-id={`menu-item-${item.category}`}
+          data-test-id={`menu-item-${item.category.toLowerCase()}`}
         >
           <h3>{item.name}</h3>
           <p>{item.price}</p>
-          <img src={item.image} alt={item.name} />
         </div>
       ))}
     </div>
   );
-};
+}
 
 export default Menu;
-
